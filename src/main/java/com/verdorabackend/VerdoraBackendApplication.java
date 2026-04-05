f2a163c2061aba9a4df33aab060ce8491c418245
Maksym Chalyi <maksymchalyi2004@gmail.com>
2026-04-05 10:21:20 +0300

    initial commit

---
+++ src/main/java/com/verdorabackend/VerdoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.verdorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdoraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verdorabackend/VerdoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verdorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
