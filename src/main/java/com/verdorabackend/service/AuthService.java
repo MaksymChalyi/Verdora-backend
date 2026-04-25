c10fa6f96d3f8da90112f0a27d2118229f234455
Maksym Chalyi <maksymchalyi2004@gmail.com>
2026-04-25 21:52:29 +0300

    change access and refresh token response for signup

--- src/main/java/com/verdorabackend/service/AuthService.java
+++ src/main/java/com/verdorabackend/service/AuthService.java
@@ -3,7 +3,6 @@ package com.verdorabackend.service;
 import com.verdorabackend.dto.auth.AuthResult;
 import com.verdorabackend.dto.request.SignInRequest;
 import com.verdorabackend.dto.request.SignUpRequest;
-import com.verdorabackend.dto.response.SignupResponse;
 
 public interface AuthService {
 
