57ac2108ca1bd79380733e72062127f08b9d4473
Viktor <vitapincuk4@gmail.com>
2026-04-25 13:29:13 +0300

    Add Category request and response

---
+++ src/main/java/com/verdorabackend/dto/response/CategoryResponse.java
@@ -0,0 +1,15 @@
+package com.verdorabackend.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Category response payload")
+public record CategoryResponse(
+
+        @Schema(description = "Category ID", example = "1")
+        String categoryId,
+
+        @Schema(description = "Category name", example = "Electronics")
+        String category
+        ) {
+
+}
